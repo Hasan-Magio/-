f8bf49527ded91c462f610885a48c9cf5a407e7b
Hasan <123621485+Hasan-Magio@users.noreply.github.com>
2023-08-18 19:03:04 +0300

    Initial Commit

---
+++ БЛОК 2 УРОК 3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  БЛОК 2 УРОК 3
+//
+//  Created by Khasan Magomaev on 18.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
